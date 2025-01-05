13397a3199e780b246af5166d587a3f48e15bab1
nb-yyds <m17133373452@163.com>
2025-01-05 17:46:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 // import signIn from './sign_in.js'
 import axios from "axios";
-import dotenv from 'dotenv'
 
 async function get_beijing_time() {}
 
 async function signIn() {
-  console.log(222, "dotenv.config()", dotenv.config())
   const json = JSON.parse(process.env.CONFIG || "{}");
   console.log("获取当前信息", json);
 }
